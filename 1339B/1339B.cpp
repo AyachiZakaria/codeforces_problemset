7ebf6995bfb7b93fc8b84f51b1cb2c5f56d1a66c
ayachi zakaria <32897527+AyachiZakaria@users.noreply.github.com>
2022-08-31 09:39:22 +0100

    1339B

---
+++ 1339B/1339B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+#define MAXN 100000
+int main()
+{
+    int n,x;
+    int t[MAXN];
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        for (int j = 0; j < x; j++)
+        {
+            cin>>t[j];
+        }
+        
+    }
+    return 0;
+}
